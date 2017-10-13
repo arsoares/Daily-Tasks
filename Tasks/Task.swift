cd03f167d7f2ab6f2bb516cb138216a83fb03ce0
Austin Soares <arsoares15@gmail.com>
2017-10-13 14:00:37 -0400

    Define TableView, Add Task File

---
+++ Tasks/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Tasks
+//
+//  Created by Austin Soares on 10/13/17.
+//  Copyright © 2017 Austin Soares. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Add protocols for Delegate and DataSource
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Look to self for the protocols
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Define number of rows in table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-
-
+    // Define what is in each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
 }
 
