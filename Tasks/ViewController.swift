2ea2e002673946fc5db6133ebc6d39dc4bd1a403
Austin Soares <arsoares15@gmail.com>
2017-10-13 14:07:17 -0400

    Add three sample tasks, display

--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -8,26 +8,49 @@
 
 import UIKit
 
-// Add protocols for Delegate and DataSource
+// 2 Add protocols for Delegate and DataSource
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Look to self for the protocols
+        // 6 build out functionality for counting tasks
+        tasks = makeTasks()
+        // 1 Look to self for the protocols
         tableView.dataSource = self
         tableView.delegate = self
     }
-    // Define number of rows in table
+    
+    // 3 Define number of rows in table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        // 7 return how many tasks are added/shown
+        return tasks.count
     }
-    // Define what is in each cell
+    // 4 Define what is in each cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Hello"
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.name
         return cell
+    }
+    // 5 create sample tasks
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Buy cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Mow lawn"
+        task3.important = false
+        
+        return [task1, task2, task3]
     }
     
 }
